9234afd0447bf941294baad70a5bb2e587e4ba1c
ayomideio <gokeayomide.tolu@gmail.com>
2021-11-05 12:31:06 +0100

    ayomideio

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -8,8 +8,7 @@ const instance = axios.create({
 
 export default instance
 
-// export const baseURL = `${process.env.REACT_APP_API_PATH}`
-// export const SOCKET_PATH = `${process.env.REACT_APP_SOCKET_PATH}`
 
-export const baseURL = 'https://chatapp-be-api.herokuapp.com/'
-export const SOCKET_PATH = 'wss://chatapp-be-api.herokuapp.com/chat/';
+
+export const baseURL = 'http://127.0.0.1:8000/'
+export const SOCKET_PATH = 'wss://127.0.0.1:8000/chat/';
